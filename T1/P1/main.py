c1d487814a9f524c675909214bb40ea180c2739b
andryu120 <andrs.carrasco@estudiante.uc.cl>
2026-09-12 22:49:56 -0300

    arreglo main

--- T1/P1/main.py
+++ T1/P1/main.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 
 path = os.getcwd()
-
+# puntos a probar
 p_rojos_amarillos = [(0, 0.0), (120, 2.0), (240, 2.0), (350, 0.0)] # apaga rojos y amarillos
 p_satura_rojos = [(10, 3.0), (60, 0.0), (120, 0.0), (240, 0.0), (340, 3.0)] #satura rojos
 p_grises = [(0, 2.0), (60, 0.0), (120, 2.0), (180, 0.0), (240, 2.0), (300, 0.0)] # colores marcados
@@ -48,35 +48,30 @@ def guardar_imagenes(imagen: tuple, numero_imagen: str , modo: str):
     
 
 if __name__ == "__main__":
-    while True:
-        modo = str(input("Elige el modo: "))
+    
+    print("imagen 1: pajaros")
 
-        print("imagen 1, pajaros")
-
-        
+    
+    imagen_original, imagen_cambiada = image("P1_IMG_2402.tif",path)
+    for titulo, p in puntos.items():
         imagen_original, imagen_cambiada = image("P1_IMG_2402.tif",path)
-        for titulo, p in puntos.items():
-            imagen_original, imagen_cambiada = image("P1_IMG_2402.tif",path)
-            img_cambiada = color_saturation(imagen_cambiada, p, modo)
-            guardar_imagenes((titulo, img_cambiada), "1", modo)
+        img_cambiada = color_saturation(imagen_cambiada, p, "hsv")
+        guardar_imagenes((titulo, img_cambiada), "1", "hsv")
 
-        
-        guardar_imagenes(("imagen_original", imagen_original), "1", modo)
+    
+    guardar_imagenes(("imagen_original", imagen_original), "1", 'hsv')
 
-        print("imagen 2, paisaje")
+    print("imagen 2, paisaje")
+    imagen_original, imagen_cambiada = image("imagen.png",path)
+    for titulo, p in puntos.items():
         imagen_original, imagen_cambiada = image("imagen.png",path)
-        for titulo, p in puntos.items():
-            imagen_original, imagen_cambiada = image("imagen.png",path)
-            img_cambiada = color_saturation(imagen_cambiada, p, modo)
-            guardar_imagenes((titulo, img_cambiada), "2", modo)
-        
-        
-        guardar_imagenes(("imagen_original", imagen_original), "2", modo)
+        img_cambiada = color_saturation(imagen_cambiada, p, "cie")
+        guardar_imagenes((titulo, img_cambiada), "2", "cie")
+    
+    
+    guardar_imagenes(("imagen_original", imagen_original), "2", "cie")
 
-        eleccion = str(input("Quieres cambiar de modo: "))
-
-        if eleccion not in ["Si", "si"]:
-            break
+        
 
 
 
