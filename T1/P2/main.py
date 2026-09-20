218264e3900048d791d0a1b9f5c2f129cc0ce2e1
andryu120 <andrs.carrasco@estudiante.uc.cl>
2026-09-19 22:21:33 -0300

    modificacion para el analisis

--- T1/P2/main.py
+++ T1/P2/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-from procesamiento import (image, mostrar_imagen, transformacion,clahe, imhist)
+from procesamiento import (image, mostrar_imagen, transformacion,clahe, imhist, mostrar_region_homogenea, transformacion_bin)
 
 import os
 path = os.getcwd()
@@ -9,11 +9,37 @@ path = os.getcwd()
 if __name__ == "__main__":
     img = image("P2_IMG_2423.png", path)
     alto_img, ancho_img = img.shape[:2]
+    #imhist(img)
+    img_modificada = transformacion(img, distancia_y=alto_img, distancia_x=ancho_img, alto_region=alto_img, ancho_region=ancho_img, control_limite= 0.0)
+    #mostrar_imagen(img_modificada)
+    imhist(img_modificada)
+
+    #img_modificada = transformacion(img, distancia_y=64, distancia_x=64, alto_region=128, ancho_region=128, control_limite= 0.0)
+    #mostrar_imagen(img_modificada)
+
+    #mostrar_region_homogenea(img,distancia_y=64, distancia_x=64, alto_region=128, ancho_region=128)
+
+    #img_clahe = clahe(img)
+    #mostrar_imagen(img_clahe)
+
+    # for i in [64,32,16]:
+    #     print("analisis distancias")
+    #     img_modificada = transformacion(img, distancia_y=i, distancia_x=i, alto_region=128, ancho_region=128, control_limite= 0.0)
+    #     mostrar_imagen(img_modificada)
+
+    # for i in [64,128,256,512]:
+    #     print("analisis overlap")
+    #     img_modificada = transformacion(img, distancia_y=64, distancia_x=64, alto_region=i, ancho_region=i, control_limite= 0.0)
+    #     mostrar_imagen(img_modificada)
+
+    # for i in [64,32,16]:
+    #     img_modificada = transformacion_bin(img, distancia_y=64, distancia_x=64, alto_region=128, ancho_region=128, control_limite= 0.0, n_bins=i)
+    #     mostrar_imagen(img_modificada)
+
     
-    img_modificada = transformacion(img, distancia_y=alto_img, distancia_x=ancho_img, alto_region=alto_img, ancho_region=ancho_img, control_limite= 0.01)
-    #img_modificada = np.clip(img_modificada, 0, 255).astype(np.uint8)
-    img_clahe = clahe(img)
-    mostrar_imagen(img_modificada)
-    mostrar_imagen(img_clahe)
+    
+
+    
+    
 
     
--- T1/P2/procesamiento.py
+++ T1/P2/procesamiento.py
@@ -22,7 +22,7 @@ def imhist(X):
     for j in range(M):
       x = X[i,j]
       h[x] = h[x]+1
-  plt.figure(figsize=(60,9))
+  plt.figure(figsize=(20,9))
   plt.bar(range(n),h[0:n])
   plt.show()
 
@@ -73,6 +73,19 @@ def calculo_cdf(region):
 
    return s_values
 
+def calculo_cdf_bin(region,n_bins):
+   # calculo para solo una region
+   M, n = region.shape[:2]
+
+   
+   hist, bin_edges = np.histogram(region,bins = n_bins, range = (0,256), density = False)
+   pdf = hist / (M*n)
+
+   cdf = np.cumsum(pdf) # sumatoria
+   s_values = np.round((n_bins-1)*cdf).astype(int)
+
+   return s_values
+
 def contraste(region, control_limite: float):
    # realiza el mismo calculo cdf pero teniendo en cuenta el contraste
    M, n = region.shape[:2]
@@ -99,10 +112,58 @@ def contraste(region, control_limite: float):
    return s_values
    
          
- 
+
+def mostrar_region_homogenea(img, distancia_y: int, distancia_x: int, alto_region: int, ancho_region: int):
+   alto_imagen, ancho_imagen = img.shape[:2]
+
+   centros_y, centros_x = malla(img, distancia_y, distancia_x)
+   min_varianza = float('inf')
+   for c_y in centros_y:
+      for c_x in centros_x:
+         region = extraer_region(img, alto_region, ancho_region, c_y, c_x)
+         varianza = np.var(region)
+         if varianza < min_varianza:
+            min_varianza = varianza
+            mejor_centro = (c_y, c_x)
+
+   c_y, c_x = mejor_centro
+   region = extraer_region(img, alto_region, ancho_region, c_y, c_x)
+   M, n = region.shape[:2]
+   n_bins = 256
+   hist, bin_edges = np.histogram(region,bins = n_bins, range = (0,256), density = False)
+   pdf = hist / (M*n)  
+   cdf = np.cumsum(pdf) # sumatoria
+
+   #mostrar los histogramas
+   fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 5))
+    
+    
+   ax1.imshow(region, cmap='gray', vmin=0, vmax=255)
+   ax1.set_title(f'Region Homogenea (Centro: Y={c_y}, X={c_x})')
+   ax1.axis('off')
+    
+   # --- Subplot 2: Histograma y CDF superpuestos ---
+   ax2.bar(range(256), hist, color='gray', label='Histograma', width=1.0)
+   ax2.set_xlabel('Nivel de Gris')
+   ax2.set_ylabel('Cantidad de Pixeles')
+    
+   # Crear un segundo eje Y (derecho) para la CDF porque tiene distinta escala (0 a 1)
+   ax3 = ax2.twinx()
+   ax3.plot(cdf, color='red', linewidth=2, label='CDF')
+   ax3.set_ylabel('Probabilidad Acumulada (CDF)')
+   ax3.set_ylim([0, 1.05])
+    
+    
+   # Juntar leyendas
+   lines_1, labels_1 = ax2.get_legend_handles_labels()
+   lines_2, labels_2 = ax3.get_legend_handles_labels()
+   ax3.legend(lines_1 + lines_2, labels_1 + labels_2, loc='center right')
+    
+   plt.show()
+
    
 
-def transformacion(img, distancia_y, distancia_x, alto_region: float, ancho_region: float, control_limite: float):
+def transformacion(img, distancia_y, distancia_x, alto_region: int, ancho_region: int, control_limite: float):
    m = np.zeros_like(img)
    centros_y, centros_x = malla(img, distancia_y, distancia_x)
    cdf_img = {}
@@ -111,7 +172,6 @@ def transformacion(img, distancia_y, distancia_x, alto_region: float, ancho_regi
       for c_x in centros_x:
          region = extraer_region(img, alto_region, ancho_region, c_y, c_x)
          if control_limite == 0.0:
-            pass
             cdf_img[(c_y, c_x)] = calculo_cdf(region)
          else:
             cdf_img[(c_y, c_x)] = contraste(region,control_limite)
@@ -183,14 +243,90 @@ def transformacion(img, distancia_y, distancia_x, alto_region: float, ancho_regi
 
    return m
 
+def transformacion_bin(img, distancia_y, distancia_x, alto_region: int, ancho_region: int, control_limite: float, n_bins):
+   m = np.zeros_like(img)
+   centros_y, centros_x = malla(img, distancia_y, distancia_x)
+   cdf_img = {}
+   # obtencion de los centros junto a su cdf
+   for c_y in centros_y:
+      for c_x in centros_x:
+         region = extraer_region(img, alto_region, ancho_region, c_y, c_x)
+         if control_limite == 0.0:
+            cdf_img[(c_y, c_x)] = calculo_cdf_bin(region, n_bins)
 
+         else:
+            cdf_img[(c_y, c_x)] = contraste(region,control_limite)
+   # ver donde caen los pixeles con relacion a los centros
+   for y in range(len(img)):
+      for x in range(len(img[y])):
+       
+       
+       v = img[y,x].astype(np.uint32)
 
+       # check centros en y
+       if y <= centros_y[0]:
 
-img = image("P2_IMG_2423.tif", os.getcwd())
+        y1 = centros_y[0]
+        y2 = centros_y[0]
+       elif y >= centros_y[-1]:
+        y1 = centros_y[-1]
+        y2 = centros_y[-1]
 
-centros_y, centros_x = malla(img, 50, 50)
+       else:
+          for i in range(1, len(centros_y)):
+            if y <= centros_y[i]:
+                y1 = centros_y[i-1]
+                y2 = centros_y[i]
+                break
+    
+          
+       # check centros en x
+       if x <= centros_x[0]:
+          x1 = centros_x[0]
+          x2 = centros_x[0]
+       elif x >= centros_x[-1]:
+          x1 = centros_x[-1]
+          x2 = centros_x[-1]
+       else:     
+          for i in range(1, len(centros_x)):
+              if x <= centros_x[i]:
+                x1 = centros_x[i-1]
+                x2 = centros_x[i]
+                break
+           
+          
 
-r1 = extraer_region(img,100, 100, centros_y[0],centros_x[0])
+      #para evitar dividir por cero
+
+       if y1 == y2:
+        peso_y1 = 1.0
+        peso_y2 = 0.0
+       else:
+        peso_y1 = (y2-y)/(y2-y1)
+        peso_y2 = (y-y1)/(y2-y1)
+       if x1 == x2:
+        peso_x1 = 1.0
+        peso_x2 = 0.0
+       else:
+        peso_x1 = (x2-x)/(x2-x1)
+        peso_x2 = (x-x1)/(x2-x1)
+
+       # mapear el nuevo bin original
+
+       v = (v*n_bins) // 256
+       v11 = cdf_img[(y1,x1)][v]
+       v12 = cdf_img[(y1,x2)][v]
+       v21 = cdf_img[(y2,x1)][v]
+       v22 = cdf_img[(y2,x2)][v]
+
+        #suma ponderada de los pesos (interpolacion bilineal)
+    
+       v_resultante = (v11*peso_y1*peso_x1)+(v12*peso_y1*peso_x2) + (v21*peso_y2*peso_x1) + (v22*peso_y2*peso_x2)
+
+       m[y,x] = v_resultante
+
+   return m
+
 
 
 # convertir la imagen a LAB y luego pasarla a RGB?
